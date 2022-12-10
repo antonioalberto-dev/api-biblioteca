9311c2e2c79c211f16cf367e0706642d00f25d04
Antonio Alberto <antonioalbertodsb@gmail.com>
2022-12-10 17:15:03 -0300

    :sparkles: GET e POST

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json()); // interpretar o que ta chegando (via POST e PUT) e convertendo para objeto
+
 const livros = [
   { id: 1, titulo: "Assassino da Capa Amarela" },
   { id: 2, titulo: "Zodiaco" },
@@ -13,6 +15,11 @@ app.get("/", (req, res) => {
 
 app.get("/livros", (req, res) => {
   res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso");
 });
 
 export default app;
