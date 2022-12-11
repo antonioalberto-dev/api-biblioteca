81af6a6f5248665130b1b2d7dfbfe624efd4b1b1
Antonio Alberto <antonioalbertodsb@gmail.com>
2022-12-11 19:13:52 -0300

    :recycle: refactor: metodo DELETE + mongodb

--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -7,5 +7,6 @@ router.get("/livros", LivroController.listarLivros);
 router.get("/livros/:id", LivroController.listarLivroPorId);
 router.post("/livros", LivroController.cadastrarLivro);
 router.put("/livros/:id", LivroController.atualizarLivro);
+router.delete("/livros/:id", LivroController.excluirLivro);
 
 export default router;
