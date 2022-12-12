15901c1d70be3290a8a9ed64e9561c996950bcf1
Antonio Alberto <antonioalbertodsb@gmail.com>
2022-12-12 14:32:27 -0300

    :recycle: refactor: adicionando relacionamento Livro - Autor

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,24 +1,29 @@
 import livros from "../models/Livro.js";
-import express from "express";
 
 class LivroController {
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
-      res.status(200).send(livros);
-    });
+    livros
+      .find()
+      .populate("autor")
+      .exec((err, livros) => {
+        res.status(200).send(livros);
+      });
   };
 
   static listarLivroPorId = (req, res) => {
     const id = req.params.id;
-    livros.findById(id, (err, livros) => {
-      if (err) {
-        res
-          .status(400)
-          .send({ message: `${err.message} - id do livro não localizado` });
-      } else {
-        res.status(200).send(livros);
-      }
-    });
+    livros
+      .findById(id)
+      .populate("autor", "nome")
+      .exec((err, livros) => {
+        if (err) {
+          res
+            .status(400)
+            .send({ message: `${err.message} - id do livro não localizado` });
+        } else {
+          res.status(200).send(livros);
+        }
+      });
   };
 
   static cadastrarLivro = (req, res) => {
